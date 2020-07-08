a1e8d93a42af1d9cc6c9e686fea55a118c1bf821
shivaramprasad <ram@shivaramprasads-MacBook-Pro.local>
2020-07-08 17:06:46 -0400

    terninary operator

--- src/Footer.js
+++ src/Footer.js
@@ -4,18 +4,25 @@ export default class Footer extends React.Component {
   constructor() {
     super();
     this.state = { fname: 'Yagnesh', lname: 'Vullam', firstName: true };
+    this.state = { pstop: 'stop', presume: 'resume', playback: true }
   }
 
   modify() {
     console.log('this.state.firstName ', this.state.firstName);
     this.setState({ firstName: !this.state.firstName });
   }
+  change() {
+    console.log('this.state.playback', this.state.playback);
+    this.setState({ playback: !this.state.playback })
+  }
 
   render() {
     return (
       <div>
         Hello {this.state.firstName ? this.state.fname : this.state.lname}
+        play{this.state.playback ? this.state.pstop : this.state.presume}
         <button onClick={() => this.modify()}>Test</button>
+        <button onClick={() => this.change()}>play</button>
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function Header(props) {
+
   const modify = () => {
     console.log('Hello');
   }
